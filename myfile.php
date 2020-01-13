4090c623356ad89218e859324a40f8f3f6a55949
Akshat Goyal <54413209+akshatgoyal-cs@users.noreply.github.com>
2020-01-13 15:36:18 +0530

    my first php code

---
+++ myfile.php
@@ -0,0 +1,10 @@
+<html style="color: blueviolet;">
+<body>
+<title> This is my nth wepage</title>
+<h1> Hello Friends How are u</h1>
+<?php
+echo "this is my first code";
+print " i am using print for display";
+?>
+</body>
+</html
